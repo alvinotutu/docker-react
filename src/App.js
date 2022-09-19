d329a08907471ae67edc10efb20b0ecb54241ddd
alvinotutu <alvinotutu@ALVINandRAYNES.localdomain>
2022-09-19 07:09:04 +0200

    Changed app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Still Here!
+          Changed some stuff!
         </p>
         <a
           className="App-link"
